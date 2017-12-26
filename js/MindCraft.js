17480daa81a27574ab3a4795a1f73041a01f6d7e
Benny Deddouch <beoit.deddouch@gmail.com>
2017-12-26 22:07:35 +0200

    matrix system

--- js/MindCraft.js
+++ js/MindCraft.js
@@ -1,9 +1,22 @@
 var world = {};
 var  sizeOfTheWorld = 20;
 
+function callback(e) {
+    var line = $(this).data('line')
+    var col = $(this).data('col')
+    console.log("  line:" + line + "  column:" + col);
+}
+
+
+function grass(x, y){
+
+    }
 
 
 //create an Array of Array (20 x 20) and set the value to ""
+
+world.createWorld = function()  {
+
 world.matrix = new Array(sizeOfTheWorld);
 
 for (var i = 0; i < world.matrix.length; i++){
@@ -20,9 +33,18 @@ for (var i = 0; i < world.matrix.length; i++) {
         for (var j = 0; j < world.matrix[i].length; j++) {
             var cell = $('<div/>')
                         .addClass('cell')
-                        .data('line', i)
-                        .data('col', j);
+                        .data('line', i)        //add line number
+                        .data('col', j);       //add colomn number
             $('#canvas').append(cell);
+            cell.on('click', callback);
         }
     }
+}
+
+world.createWorld();
+
+
+var sel = $("div[line='19']");
+sel.css("background-color", "yellow");
+
 
